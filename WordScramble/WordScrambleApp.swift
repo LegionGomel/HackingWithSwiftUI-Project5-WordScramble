669a1e2b8402558caaea4e7aaa3861e6bec5bb3f
Yuri Ershov <shtorm0606@gmail.com>
2022-01-12 20:53:42 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Yuri Ershov on 12.01.22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
